2c6d69a349b4a7db7b6f1fe44bf701e9cfbd353e
Atikah <atikahnazir6@gmail.com>
2023-06-20 18:21:06 +0800

    fetch data

--- app.py
+++ app.py
@@ -2,9 +2,39 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Data Analyst',
+        'location': 'KL',
+        'salary': 'RM5000'
+    },
+    {
+        'id': 2,
+        'title': 'Data Scientist',
+        'location': 'KL',
+        'salary': 'RM4000'
+    },
+    {
+        'id': 3,
+        'title': 'Back End Engineer',
+        'location': 'KL',
+
+    },
+    {
+        'id': 4,
+        'title': 'Front End engineer',
+        'location': 'KL',
+        'salary': 'RM4000'
+        
+    }
+
+]
+
+
 @app.route("/")
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html', jobs=JOBS, company_name='Atikah')
 
 if __name__ == "__main__":
     app.run('0.0.0.0', debug= True)
