e8f7d56ed92155dd9e48fe0831785db5b5db8f85
Atikah <atikahnazir6@gmail.com>
2023-06-20 23:51:27 +0800

    dynamic data and route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -35,6 +35,11 @@ JOBS = [
 @app.route("/")
 def hello_world():
     return render_template('home.html', jobs=JOBS, company_name='Atikah')
+
+# also can use route from api
+@app.route("/api/jobs")
+def list_jobs():
+    return jsonify(JOBS)
 
 if __name__ == "__main__":
     app.run('0.0.0.0', debug= True)
